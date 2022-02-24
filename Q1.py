9b80efc2bb50c132e457a0f2e30ca95a7d56931d
Jirapad <85248460+Jirapad@users.noreply.github.com>
2022-02-24 18:29:17 +0700

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -53,6 +53,30 @@ class Simple_drawing_window2(QWidget):
         p.drawPolygon(points)
         p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
+
+class Simple_drawing_window3(QWidget):
+    def __init__(self):
+        QWidget.__init__(self, None)
+        self.setWindowTitle("Simple Drawing")
+        self.rabbit = QPixmap("images/rabbit.png")
+
+    def paintEvent(self, e):
+        p = QPainter()
+        p.begin(self)
+
+        p.setPen(QColor(0, 0, 0))
+        p.setBrush(QColor(0, 127, 0))
+        p.drawPolygon([
+            QPoint( 70, 100), QPoint(100, 110),
+            QPoint(100, 150),
+        ])
+
+        p.setPen(QColor(255, 127, 0))
+        p.setBrush(QColor(255, 127, 0))
+        p.drawPie(50, 150, 100, 100, 0, 360 * 16)
+
+        p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
+        p.end()
  
 def main():
     app = QApplication(sys.argv)
@@ -61,6 +85,8 @@ def main():
     w.show()
     x = Simple_drawing_window2()
     x.show()
+    y = Simple_drawing_window3()
+    y.show()
 
     return app.exec_()
 
