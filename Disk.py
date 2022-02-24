0b1f46301b428f350507fc638f6f1c12134fe481
NonKrit <krittawa.t3004@gmail.com>
2022-02-24 19:16:35 +0700

    Q3

--- Disk.py
+++ Disk.py
@@ -3,11 +3,11 @@ from turtle import *
 
 class Disk(object):
     def __init__(self, name="", x_pos=0, y_pos=0, height=20, width=40):
-        self.n = name
+        self.name = name
         self.x_pos = x_pos
         self.y_pos = y_pos
-        self.disk_h = height
-        self.disk_w = width
+        self.disk_height = height
+        self.disk_width = width
 
     def show_disk(self):
         left(90)
@@ -16,11 +16,11 @@ class Disk(object):
         pendown()
         rt(90)
         for x in range(2):
-            forward(self.disk_w/2)
+            forward(self.disk_width/2)
             left(90)
-            forward(self.disk_h)
+            forward(self.disk_height)
             left(90)
-            forward(self.disk_w/2)
+            forward(self.disk_width/2)
 
     def new_pos(self, x_pos, y_pos):
         self.x_pos = x_pos
