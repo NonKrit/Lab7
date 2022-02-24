69776c81bc26a1a1dfda0eacbb772ba781bc48ad
NonKrit <krittawa.t3004@gmail.com>
2022-02-24 18:33:26 +0700

    Update Q1.py

--- Q1.py
+++ Q1.py
@@ -3,7 +3,7 @@ from PySide6.QtCore import *
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
 
-class Simple_drawing_window(QWidget):
+class Simple_drawing_window1(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
@@ -12,22 +12,18 @@ class Simple_drawing_window(QWidget):
     def paintEvent(self, e):
         p = QPainter()
         p.begin(self)
- 
+
         p.setPen(QColor(0, 0, 0))
         p.setBrush(QColor(0, 127, 0))
         p.drawPolygon([
             QPoint( 70, 100), QPoint(100, 110),
-            QPoint(130, 100), QPoint(100, 150),
+            QPoint(100, 150),
         ])
- 
-        p.setPen(QColor(255, 127, 0))
-        p.setBrush(QColor(255, 127, 0))
-        p.drawPie(50, 150, 100, 100, 0, 180 * 16)
-        
-        p.drawPolygon(
-            [QPoint( 50, 200), QPoint(150, 200), QPoint(100, 400),]
-            )
- 
+
+        p.setPen(QColor(255, 0, 0))
+        p.setBrush(QColor(255, 0, 0))
+        p.drawPie(50, 150, 100, 100, 0, 360 * 16)
+
         p.drawPixmap(QRect(200, 100, 320, 320), self.rabbit)
         p.end()
      
@@ -58,7 +54,7 @@ class Simple_drawing_window3(QWidget):
     def __init__(self):
         QWidget.__init__(self, None)
         self.setWindowTitle("Simple Drawing")
-        self.rabbit = QPixmap("images/rabbit.png")
+        self.rabbit = QPixmap("rabbit.png")
 
     def paintEvent(self, e):
         p = QPainter()
@@ -81,7 +77,7 @@ class Simple_drawing_window3(QWidget):
 def main():
     app = QApplication(sys.argv)
  
-    w = Simple_drawing_window()
+    w = Simple_drawing_window1()
     w.show()
     x = Simple_drawing_window2()
     x.show()
